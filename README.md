71c0341f2fd247fcc9b6ec02e2739e64fa8d11b6
Rajtanu Chakravarty <rajtanuchakravarty@gmail.com>
2023-02-16 21:26:58 +0530

    my source code for the sixth challenge "Checkout Page" of devChallenges.io

---
+++ README.md
@@ -0,0 +1,68 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">Recipe Page</h1>
+
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://rajtanu1.github.io/Checkout-Page-devChallenges">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/solutions/R5lBpyO3osVHRI2pvp3K">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/0J1NxxGhOUYVqihwegfO">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+- Where can you see a demo?
+ 
+  **Answer** : You can find the demo of this project at [Checkout Page](https://rajtanu1.github.io/Checkout-Page-devChallenges).
+
+- What was your experience?
+ 
+  **Answer** : Had experience in creating a toast(feedback to an action) using javascript on valid user details.
+
+- What you learned/improved?
+  
+  **Answer** : Learned to write a bit better javascript now.
+
+### Built With
+
+<!-- This section should list technologies that you built your project using. Here are a few examples.-->
+
+- [HTML5](https://html5.org/)
+- [CSS3](https://css3.com/)
+- [Javascript](https://javascript.com/)
+
+## Features
+
+<!-- List the features of your application or follow the template. Don't share the figma file here :) -->
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/0J1NxxGhOUYVqihwegfO) was to build an application to complete the given user stories.
+
+## Contact
+
+- Website [Check my website!](https://rajtanuchakravarty.onrender.com)
+- GitHub [Go to my github.](https://github.com/Rajtanu1)
+
---
+++ index-script.js
@@ -0,0 +1,110 @@
+let productOneQuantityContainer = document.querySelector(".product-one__quantity-container");
+let productTwoQuantityContainer = document.querySelector(".product-two__quantity-container");
+let buyerForm = document.querySelector(".buyer-form");
+let dropdownBox = document.querySelector(".drop-down-box");
+let checkbox = document.querySelector(".checkbox");
+let dropdownIcon = document.querySelector(".drop-down-icon");
+let checkIcon = document.querySelector(".fa-check");
+let inputWithIdCountry = document.querySelector("#country");
+let validationPopup = document.querySelector(".validation-popup");
+
+//custom functions
+function checkElementVisibility(element) {
+  if (element.classList.contains("visible")) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function toggleElementVisibilityClassName(element) {
+  if (element.classList.contains("visible")) {
+    element.classList.remove("visible");
+    element.classList.add("invisible");
+  } else {
+    element.classList.add("visible");
+    element.classList.remove("invisible");
+  }
+};
+
+function addSlideInOutAnimationToValidationPopup() {
+  validationPopup.style.animationName = "slide-in-out";
+};
+
+function removeSlideInOutAnimationFromValidationPopup() {
+  validationPopup.style.animationName = "";
+};
+
+function emptyFormInputFields() {
+  let counter = buyerForm.length - 1; 
+  
+  while (counter > -1) {
+    buyerForm[counter].value = "";
+    counter--;
+  }
+};
+
+productOneQuantityContainer.addEventListener("click", function(event) {
+  let clickedElement = event.target;
+  let productQuantityElement = productOneQuantityContainer.children[1];
+  let productQuantityElementTextContent = Number(productQuantityElement.textContent);
+
+  if (clickedElement.className === "increment-button") {
+    productQuantityElement.textContent = productQuantityElementTextContent + 1;
+  } else if (clickedElement.className === "decrement-button" && productQuantityElementTextContent > 0) {
+    productQuantityElement.textContent = productQuantityElementTextContent - 1;
+  }
+});
+
+productTwoQuantityContainer.addEventListener("click", function(event) {
+  let clickedElement = event.target;
+  let productQuantityElement = productTwoQuantityContainer.children[1];
+  let productQuantityElementTextContent = Number(productQuantityElement.textContent);
+
+  if (clickedElement.className === "increment-button") {
+    productQuantityElement.textContent = productQuantityElementTextContent + 1;
+  } else if (clickedElement.className === "decrement-button" && productQuantityElementTextContent > 0) {
+    productQuantityElement.textContent = productQuantityElementTextContent - 1;
+  }
+});
+
+dropdownBox.addEventListener("click", function(event) {
+  let clickedElement = event.target;
+
+  if (clickedElement.className === "drop-down-option") {
+    inputWithIdCountry.value = clickedElement.textContent;
+  }
+});
+
+dropdownIcon.addEventListener("click", function(event) {
+  let dropdownBoxIsVisible = checkElementVisibility(dropdownBox);
+  
+  if (dropdownBoxIsVisible === true) {
+    toggleElementVisibilityClassName(dropdownBox);
+    dropdownBox.style.transform = "scale(0)";
+  } else {
+    toggleElementVisibilityClassName(dropdownBox);
+    dropdownBox.style.transform = "scale(1)";
+  }
+});
+
+checkIcon.addEventListener("click", function(event) {
+  let isCheckIconVisible = checkElementVisibility(checkIcon);
+
+  if (isCheckIconVisible === true) {
+    toggleElementVisibilityClassName(checkIcon);
+    checkIcon.style.opacity = "0";
+    checkbox.style.borderColor = "black";
+  } else {
+    toggleElementVisibilityClassName(checkIcon);
+    checkIcon.style.opacity = "1";
+    checkbox.style.borderColor = "white";
+  }
+})
+
+buyerForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  addSlideInOutAnimationToValidationPopup();
+  emptyFormInputFields();
+  setTimeout(removeSlideInOutAnimationFromValidationPopup, 3000);
+});
